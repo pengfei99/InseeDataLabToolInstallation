c73bc8459a6f69f07f571a7129c62f4ab2dc5bf7
pengfei <liu.pengfei@hotmail.fr>
2021-08-02 17:23:46 +0200

    update argo client

--- ubuntu_vm/argo_deb_install.sh
+++ ubuntu_vm/argo_deb_install.sh
@@ -1,14 +1,14 @@
-# Download the binary
-curl -sLO https://github.com/argoproj/argo/releases/download/v3.0.0-rc8/argo-linux-amd64.gz
+# Download the binary from https://github.com/argoproj/argo-workflows/releases/latest
+curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.1.3/argo-darwin-amd64.gz
 
 # Unzip
-gunzip argo-linux-amd64.gz
+gunzip argo-darwin-amd64.gz
 
 # Make binary executable
-chmod +x argo-linux-amd64
+chmod +x argo-darwin-amd64
 
 # Move binary to path
-mv ./argo-linux-amd64 /usr/local/bin/argo
+mv ./argo-darwin-amd64 /usr/local/bin/argo
 
 # Test installation
 argo version
